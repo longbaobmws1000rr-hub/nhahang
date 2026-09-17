2bb88c333ec327bfcb19706c1b2d8ace0e5af851
longbaobmws1000rr-hub <longbaobmws1000rr@gmail.com>
2026-09-17 16:10:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 st.set_page_config(page_title="Order Nhà Hàng", layout="wide")
 
 st.image("Unknown-2.jpg")
-st.title("🍽️ Hệ thống Order Nhà Hàng - VŨ ĐỨC BÌNH")
+st.title("🍽️ Hệ thống Order Nhà Hàng - LONG ODYSSEY FOOD")
 
 # ==========================
 # Khởi tạo dữ liệu
